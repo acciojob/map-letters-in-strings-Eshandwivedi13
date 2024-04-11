45f5bb4b23366c641d71f5a4f078042626b72669
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-11 22:20:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function mapLetters(str) {
+	let obj = {};
+	for(let i = 0; i< str.length; i++){
+		let ch = str[i];
+		if(obj[ch] == undefined){
+			obj[ch] = [i];
+		}else{
+			obj[ch].push(i);
+		}
+	}
+}
+
+
+// const str = prompt("Enter str: ");
+alert(JSON.stringify(mapLetters(str)));
