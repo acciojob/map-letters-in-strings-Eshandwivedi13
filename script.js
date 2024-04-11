56eeef210f48357a1c6f8c7f5d9961039fb68969
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-11 22:25:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function mapLetters(str) {
-	let obj = {};
+	let obj = {};//in object duplicate keys are not allowed
 	for(let i = 0; i< str.length; i++){
-		let ch = str[i];
-		if(obj[ch] == undefined){
+		let ch = str[i];// or str.charAt(i)
+		if(obj[ch] == undefined){//if this character is not present
 			obj[ch] = [i];
+//put the value as array, key also =>> add as obj[ch] not obj.ch cuz 'ch' varaible is not present in obj....its actually storing characters			
 		}else{
-			obj[ch].push(i);
+			obj[ch].push(i);//if character already present, push ith index also
 		}
 	}
+	return obj;
 }
 
 
-// const str = prompt("Enter str: ");
+const str = prompt("Enter str: ");
 alert(JSON.stringify(mapLetters(str)));
